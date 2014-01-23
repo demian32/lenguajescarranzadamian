c5f39b8b0da31deceb68dd786b962d4de73451cf
damian <damian_carranza@hotmail.com>
2014-01-23 14:29:17 -0600

    Update Lenjuagescarranzadamian.java

--- Lenjuagescarranzadamian/src/lenjuagescarranzadamian/Lenjuagescarranzadamian.java
+++ Lenjuagescarranzadamian/src/lenjuagescarranzadamian/Lenjuagescarranzadamian.java
@@ -16,7 +16,7 @@ public class Lenjuagescarranzadamian {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         System.out.println("Bienvenidos al curso de lenguajes!!!");
+         System.out.println("Bienveeenidos al curso de lenguajes!!!");
          System.out.println("Algo mal");
         
     
