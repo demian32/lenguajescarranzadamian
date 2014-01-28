5fbb6b7962d214089bf52e1385c816438048ceba
T107 <T107@PC257>
2014-01-27 19:38:13 -0600

    clase2

---
+++ Lenjuagescarranzadamian/src/clase2/Testusuario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clase2;
+
+/**
+ *
+ * @author T107
+ */
+public class Testusuario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Usuario edwin=new Usuario();
+        edwin.setEdad(11);
+        edwin.getEdad();
+        
+    }
+    
+}
