6c3077da797cc7e8b99b1c17c6196aeb567238b8
Deyanira <Deyanira@Deyanira.lan>
2014-03-12 00:02:02 -0600

    tareatarjeta

--- Lenjuagescarranzadamian/src/Capitulo5/PrimerThread.java
+++ Lenjuagescarranzadamian/src/Capitulo5/PrimerThread.java
@@ -23,20 +23,27 @@ public class PrimerThread implements Runnable {
         Runnable r=new PrimerThread();
         Thread t1=new Thread(r);
         Thread t2=new Thread(r);
+        Thread t3=new Thread(r);
+        Thread t4=new Thread(r);
         t1.setName("primero");
         t2.setName("segundo");
+        t3.setName("tercero");
+        t4.setName("cuarto");
         //2.inicializacion
        t1.start();
        t2.start();
-        
+       t3.start();
+       t4.start();
         
     }
 //3.en ejecucion
     public void run() {
         try {
-            if(Thread.currentThread().getName().equals("primero"))Thread.sleep(4000);
+            if(Thread.currentThread().getName().equals("segundo"))Thread.sleep(3000);
+            if(Thread.currentThread().getName().equals("tercero"))Thread.sleep(5000);
+            if(Thread.currentThread().getName().equals("cuarto"))Thread.sleep(8000);
             
-            System.out.println("soy un thread"+ Thread.currentThread().getName());
+            System.out.println("soy un thread  "+ Thread.currentThread().getName());
         } catch (InterruptedException ex) {
         }
     }
