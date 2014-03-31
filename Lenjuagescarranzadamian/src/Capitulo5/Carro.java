13f385320899eb00e4aa48de38a1baa8ecb1115c
Deyanira <Deyanira@Deyanira.lan>
2014-03-30 20:51:26 -0600

    tareatarjeta

--- Lenjuagescarranzadamian/src/Capitulo5/Carro.java
+++ Lenjuagescarranzadamian/src/Capitulo5/Carro.java
@@ -10,8 +10,29 @@ package Capitulo5;
 public class Carro {
 
     public static void main(String[] args) {
-        Random rnd = new Random();
-                 System.out.println(rnd.nextInt(1+1+1)+-1);
+        
+        
+        Thread t=new  Thread (new Runnable(){
+            public int x;
+            public int y;
+            public void run(){
+                while(true){
+                try {
+                    Thread.sleep(5000);
+                    //creamos un objeto de una clase que se llama calendar
+                    Random rnd = new Random();
+                    
+                    x=(rnd.nextInt(1+1+1)+-1);
+                    y=(rnd.nextInt(1+1+1)+-1);
+                    
+                    System.out.println(x+" "+y);
+                    
+                } catch (InterruptedException ex){
+                }}
+            }
+        });
+        t.start();
+         
     }                                                                                                              
     
 }
